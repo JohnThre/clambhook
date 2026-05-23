5177232595fc262bc32e33eef3e195e84825fed7
Pengfan Chang <developer@jpfchang.org>
2026-05-23 17:29:13 +0800

    Add release packaging smoke checks

--- cmd/clambhook-tui/main.go
+++ cmd/clambhook-tui/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -10,9 +11,17 @@ import (
 var version = "dev"
 
 func main() {
+	showVersion := flag.Bool("version", false, "print version and exit")
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Printf("clambhook-tui %s\n", version)
+		os.Exit(0)
+	}
+
 	apiAddr := "127.0.0.1:9090"
-	if len(os.Args) > 1 {
-		apiAddr = os.Args[1]
+	if flag.NArg() > 0 {
+		apiAddr = flag.Arg(0)
 	}
 
 	p := tea.NewProgram(newModel(apiAddr))
