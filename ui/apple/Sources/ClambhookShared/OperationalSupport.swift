ea661d12f2e321750981ceac4a04437f8c4744e3
Pengfan Chang <developer@jpfchang.org>
2026-06-01 18:46:01 +0800

    Implement typed profile templates

--- ui/apple/Sources/ClambhookShared/OperationalSupport.swift
+++ ui/apple/Sources/ClambhookShared/OperationalSupport.swift
@@ -55,12 +55,412 @@ public enum TunnelImportDecoder {
     }
 }
 
+public enum TunnelProfileTemplate: String, CaseIterable, Codable, Identifiable, Sendable {
+    case shadowsocks
+    case wireguard
+    case openvpn
+    case trojan
+    case tor
+    case clambback
+    case advanced
+
+    public var id: String { rawValue }
+
+    public var displayName: String {
+        switch self {
+        case .shadowsocks:
+            return "Shadowsocks"
+        case .wireguard:
+            return "WireGuard"
+        case .openvpn:
+            return "OpenVPN"
+        case .trojan:
+            return "Trojan"
+        case .tor:
+            return "Tor"
+        case .clambback:
+            return "Clambback"
+        case .advanced:
+            return "Advanced"
+        }
+    }
+
+    public var protocolName: String? {
+        self == .advanced ? nil : rawValue
+    }
+
+    public var defaultServerName: String {
+        switch self {
+        case .advanced:
+            return "server"
+        default:
+            return rawValue
+        }
+    }
+}
+
+public enum TunnelProfileSettingValue: Codable, Equatable, Sendable {
+    case string(String)
+    case bool(Bool)
+    case int(Int)
+    case double(Double)
+    case array([TunnelProfileSettingValue])
+    case object([String: TunnelProfileSettingValue])
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let value = try? container.decode(Bool.self) {
+            self = .bool(value)
+        } else if let value = try? container.decode(Int.self) {
+            self = .int(value)
+        } else if let value = try? container.decode(Double.self) {
+            self = .double(value)
+        } else if let value = try? container.decode(String.self) {
+            self = .string(value)
+        } else if let value = try? container.decode([TunnelProfileSettingValue].self) {
+            self = .array(value)
+        } else {
+            self = .object(try container.decode([String: TunnelProfileSettingValue].self))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .string(let value):
+            try container.encode(value)
+        case .bool(let value):
+            try container.encode(value)
+        case .int(let value):
+            try container.encode(value)
+        case .double(let value):
+            try container.encode(value)
+        case .array(let value):
+            try container.encode(value)
+        case .object(let value):
+            try container.encode(value)
+        }
+    }
+}
+
+public struct TunnelShadowsocksTemplateSettings: Equatable, Sendable {
+    public var method: String
+    public var password: String
+
+    public init(method: String = "chacha20-ietf-poly1305", password: String = "") {
+        self.method = method
+        self.password = password
+    }
+
+    public var settings: [String: TunnelProfileSettingValue] {
+        [
+            "method": .string(method.trimmedForProfileTemplate),
+            "password": .string(password),
+        ]
+    }
+}
+
+public struct TunnelWireGuardTemplateSettings: Equatable, Sendable {
+    public var privateKey: String
+    public var interfaceAddresses: String
+    public var dnsServers: String
+    public var peerPublicKey: String
+    public var presharedKey: String
+    public var allowedIPs: String
+    public var persistentKeepalive: Int
+    public var mtu: Int
+    public var logLevel: String
+
+    public init(
+        privateKey: String = "",
+        interfaceAddresses: String = "10.0.0.2/32",
+        dnsServers: String = "",
+        peerPublicKey: String = "",
+        presharedKey: String = "",
+        allowedIPs: String = "0.0.0.0/0, ::/0",
+        persistentKeepalive: Int = 25,
+        mtu: Int = 1420,
+        logLevel: String = "error"
+    ) {
+        self.privateKey = privateKey
+        self.interfaceAddresses = interfaceAddresses
+        self.dnsServers = dnsServers
+        self.peerPublicKey = peerPublicKey
+        self.presharedKey = presharedKey
+        self.allowedIPs = allowedIPs
+        self.persistentKeepalive = persistentKeepalive
+        self.mtu = mtu
+        self.logLevel = logLevel
+    }
+
+    public func settings(endpoint: String) -> [String: TunnelProfileSettingValue] {
+        var peer: [String: TunnelProfileSettingValue] = [
+            "public_key": .string(peerPublicKey.trimmedForProfileTemplate),
+            "endpoint": .string(endpoint.trimmedForProfileTemplate),
+            "allowed_ips": .array(TunnelProfileCreateDraft.stringListValues(from: allowedIPs)),
+        ]
+        if !presharedKey.trimmedForProfileTemplate.isEmpty {
+            peer["preshared_key"] = .string(presharedKey.trimmedForProfileTemplate)
+        }
+        if persistentKeepalive > 0 {
+            peer["persistent_keepalive"] = .int(persistentKeepalive)
+        }
+
+        var settings: [String: TunnelProfileSettingValue] = [
+            "private_key": .string(privateKey.trimmedForProfileTemplate),
+            "addresses": .array(TunnelProfileCreateDraft.stringListValues(from: interfaceAddresses)),
+            "peers": .array([.object(peer)]),
+            "log_level": .string(logLevel.trimmedForProfileTemplate),
+        ]
+        let dns = TunnelProfileCreateDraft.stringListValues(from: dnsServers)
+        if !dns.isEmpty {
+            settings["dns"] = .array(dns)
+        }
+        if mtu > 0 {
+            settings["mtu"] = .int(mtu)
+        }
+        return settings
+    }
+}
+
+public struct TunnelOpenVPNTemplateSettings: Equatable, Sendable {
+    public var caCert: String
+    public var clientCert: String
+    public var clientKey: String
+    public var serverCN: String
+    public var username: String
+    public var password: String
+    public var cipher: String
+    public var tunMTU: Int
+    public var skipCertVerify: Bool
+
+    public init(
+        caCert: String = "",
+        clientCert: String = "",
+        clientKey: String = "",
+        serverCN: String = "",
+        username: String = "",
+        password: String = "",
+        cipher: String = "",
+        tunMTU: Int = 1500,
+        skipCertVerify: Bool = false
+    ) {
+        self.caCert = caCert
+        self.clientCert = clientCert
+        self.clientKey = clientKey
+        self.serverCN = serverCN
+        self.username = username
+        self.password = password
+        self.cipher = cipher
+        self.tunMTU = tunMTU
+        self.skipCertVerify = skipCertVerify
+    }
+
+    public var settings: [String: TunnelProfileSettingValue] {
+        var settings: [String: TunnelProfileSettingValue] = [
+            "ca_cert": .string(caCert),
+            "client_cert": .string(clientCert),
+            "client_key": .string(clientKey),
+            "skip_cert_verify": .bool(skipCertVerify),
+        ]
+        if !serverCN.trimmedForProfileTemplate.isEmpty {
+            settings["server_cn"] = .string(serverCN.trimmedForProfileTemplate)
+        }
+        if !username.trimmedForProfileTemplate.isEmpty || !password.isEmpty {
+            settings["username"] = .string(username.trimmedForProfileTemplate)
+            settings["password"] = .string(password)
+        }
+        if !cipher.trimmedForProfileTemplate.isEmpty {
+            settings["cipher"] = .string(cipher.trimmedForProfileTemplate)
+        }
+        if tunMTU > 0 {
+            settings["tun_mtu"] = .int(tunMTU)
+        }
+        return settings
+    }
+}
+
+public struct TunnelTrojanTemplateSettings: Equatable, Sendable {
+    public var password: String
+    public var sni: String
+    public var alpn: String
+    public var skipCertVerify: Bool
+
+    public init(password: String = "", sni: String = "", alpn: String = "", skipCertVerify: Bool = false) {
+        self.password = password
+        self.sni = sni
+        self.alpn = alpn
+        self.skipCertVerify = skipCertVerify
+    }
+
+    public var settings: [String: TunnelProfileSettingValue] {
+        var settings: [String: TunnelProfileSettingValue] = [
+            "password": .string(password),
+            "skip_cert_verify": .bool(skipCertVerify),
+        ]
+        if !sni.trimmedForProfileTemplate.isEmpty {
+            settings["sni"] = .string(sni.trimmedForProfileTemplate)
+        }
+        let alpnValues = TunnelProfileCreateDraft.stringListValues(from: alpn)
+        if !alpnValues.isEmpty {
+            settings["alpn"] = .array(alpnValues)
+        }
+        return settings
+    }
+}
+
+public struct TunnelTorTemplateSettings: Equatable, Sendable {
+    public var isolationUser: String
+    public var isolationPass: String
+
+    public init(isolationUser: String = "", isolationPass: String = "") {
+        self.isolationUser = isolationUser
+        self.isolationPass = isolationPass
+    }
+
+    public var settings: [String: TunnelProfileSettingValue] {
+        var settings: [String: TunnelProfileSettingValue] = [:]
+        if !isolationUser.trimmedForProfileTemplate.isEmpty || !isolationPass.isEmpty {
+            settings["isolation_user"] = .string(isolationUser.trimmedForProfileTemplate)
+            settings["isolation_pass"] = .string(isolationPass)
+        }
+        return settings
+    }
+}
+
+public struct TunnelProfileCreateDraft: Equatable, Sendable {
+    public var template: TunnelProfileTemplate
+    public var profileName: String
+    public var chainName: String
+    public var serverName: String
+    public var serverAddress: String
+    public var replace: Bool
+    public var shadowsocks: TunnelShadowsocksTemplateSettings
+    public var wireguard: TunnelWireGuardTemplateSettings
+    public var openvpn: TunnelOpenVPNTemplateSettings
+    public var trojan: TunnelTrojanTemplateSettings
+    public var tor: TunnelTorTemplateSettings
+    public var clambback: TunnelTrojanTemplateSettings
+    public var advancedTOML: String
+
+    public init(
+        template: TunnelProfileTemplate = .shadowsocks,
+        profileName: String = "default",
+        chainName: String = "proxy",
+        serverName: String = "server",
+        serverAddress: String = "",
+        replace: Bool = true,
+        shadowsocks: TunnelShadowsocksTemplateSettings = TunnelShadowsocksTemplateSettings(),
+        wireguard: TunnelWireGuardTemplateSettings = TunnelWireGuardTemplateSettings(),
+        openvpn: TunnelOpenVPNTemplateSettings = TunnelOpenVPNTemplateSettings(),
+        trojan: TunnelTrojanTemplateSettings = TunnelTrojanTemplateSettings(),
+        tor: TunnelTorTemplateSettings = TunnelTorTemplateSettings(),
+        clambback: TunnelTrojanTemplateSettings = TunnelTrojanTemplateSettings(),
+        advancedTOML: String = ""
+    ) {
+        self.template = template
+        self.profileName = profileName
+        self.chainName = chainName
+        self.serverName = serverName
+        self.serverAddress = serverAddress
+        self.replace = replace
+        self.shadowsocks = shadowsocks
+        self.wireguard = wireguard
+        self.openvpn = openvpn
+        self.trojan = trojan
+        self.tor = tor
+        self.clambback = clambback
+        self.advancedTOML = advancedTOML
+    }
+
+    public var isInputComplete: Bool {
+        switch template {
+        case .advanced:
+            return !advancedTOML.trimmedForProfileTemplate.isEmpty
+        case .shadowsocks:
+            return hasCommonCreateFields && !shadowsocks.password.isEmpty
+        case .wireguard:
+            return hasCommonCreateFields &&
+                !wireguard.privateKey.trimmedForProfileTemplate.isEmpty &&
+                !wireguard.peerPublicKey.trimmedForProfileTemplate.isEmpty &&
+                !Self.stringListValues(from: wireguard.interfaceAddresses).isEmpty &&
+                !Self.stringListValues(from: wireguard.allowedIPs).isEmpty
+        case .openvpn:
+            return hasCommonCreateFields &&
+                !openvpn.caCert.trimmedForProfileTemplate.isEmpty &&
+                !openvpn.clientCert.trimmedForProfileTemplate.isEmpty &&
+                !openvpn.clientKey.trimmedForProfileTemplate.isEmpty
+        case .trojan:
+            return hasCommonCreateFields && !trojan.password.isEmpty
+        case .tor:
+            return hasCommonCreateFields &&
+                (tor.isolationUser.trimmedForProfileTemplate.isEmpty == tor.isolationPass.isEmpty)
+        case .clambback:
+            return hasCommonCreateFields && !clambback.password.isEmpty
+        }
+    }
+
+    public mutating func applyTemplateDefaults(previousTemplate: TunnelProfileTemplate) {
+        if serverName.trimmedForProfileTemplate.isEmpty || serverName == previousTemplate.defaultServerName || serverName == "server" {
+            serverName = template.defaultServerName
+        }
+        if template == .tor && serverAddress.trimmedForProfileTemplate.isEmpty {
+            serverAddress = "127.0.0.1:9050"
+        }
+    }
+
+    public func makeCreateRequest() -> TunnelProfileCreateRequest? {
+        guard let protocolName = template.protocolName else {
+            return nil
+        }
+        return TunnelProfileCreateRequest(
+            profileName: profileName.trimmedForProfileTemplate,
+            chainName: chainName.trimmedForProfileTemplate,
+            serverName: serverName.trimmedForProfileTemplate,
+            protocol: protocolName,
+            serverAddress: serverAddress.trimmedForProfileTemplate,
+            settingsTOML: "",
+            settings: settings,
+            replace: replace
+        )
+    }
+
+    public static func stringListValues(from rawValue: String) -> [TunnelProfileSettingValue] {
+        rawValue.profileTemplateListItems.map { .string($0) }
+    }
+
+    private var hasCommonCreateFields: Bool {
+        !profileName.trimmedForProfileTemplate.isEmpty &&
+            !serverAddress.trimmedForProfileTemplate.isEmpty
+    }
+
+    private var settings: [String: TunnelProfileSettingValue] {
+        switch template {
+        case .shadowsocks:
+            return shadowsocks.settings
+        case .wireguard:
+            return wireguard.settings(endpoint: serverAddress)
+        case .openvpn:
+            return openvpn.settings
+        case .trojan:
+            return trojan.settings
+        case .tor:
+            return tor.settings
+        case .clambback:
+            return clambback.settings
+        case .advanced:
+            return [:]
+        }
+    }
+}
+
 public struct TunnelProfileCreateRequest: Codable, Equatable, Sendable {
     public var profileName: String
     public var chainName: String
     public var serverName: String
     public var serverAddress: String
     public var `protocol`: String
+    public var settings: [String: TunnelProfileSettingValue]?
     public var settingsTOML: String
     public var replace: Bool
 
@@ -70,16 +470,27 @@ public struct TunnelProfileCreateRequest: Codable, Equatable, Sendable {
         case serverName = "server_name"
         case serverAddress = "server_address"
         case `protocol`
+        case settings
         case settingsTOML = "settings_toml"
         case replace
     }
 
-    public init(profileName: String = "default", chainName: String = "proxy", serverName: String = "server", protocol: String = "shadowsocks", serverAddress: String = "", settingsTOML: String = "", replace: Bool = true) {
+    public init(
+        profileName: String = "default",
+        chainName: String = "proxy",
+        serverName: String = "server",
+        protocol: String = "shadowsocks",
+        serverAddress: String = "",
+        settingsTOML: String = "",
+        settings: [String: TunnelProfileSettingValue]? = nil,
+        replace: Bool = true
+    ) {
         self.profileName = profileName
         self.chainName = chainName
         self.serverName = serverName
         self.protocol = `protocol`
         self.serverAddress = serverAddress
+        self.settings = settings
         self.settingsTOML = settingsTOML
         self.replace = replace
     }
@@ -212,6 +623,18 @@ public extension TunnelConfigStore {
 
 private func hopMatchesServer(_ hop: TrafficHopPayload, server: ServerPayload) -> Bool {
     hop.address == server.address || (!hop.name.isEmpty && hop.name == server.name)
+}
+
+private extension String {
+    var trimmedForProfileTemplate: String {
+        trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+
+    var profileTemplateListItems: [String] {
+        components(separatedBy: CharacterSet(charactersIn: ",\n"))
+            .map { $0.trimmedForProfileTemplate }
+            .filter { !$0.isEmpty }
+    }
 }
 
 private extension Data {
